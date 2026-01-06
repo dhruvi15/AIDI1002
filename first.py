af097f71d9ee59196e9f0e27e6203e260c444047
dhruvi15 <dhruvikachhadiya15@gmail.com>
2026-01-06 14:06:24 -0500

    First commit

---
+++ first.py
@@ -0,0 +1,18 @@
+# STEP 1: Create different data types
+name =  "Dhrvi"
+age =  25
+gpa =  3.9
+ 
+# STEP 2: Print data types
+
+print(name)
+print(age)
+print(gpa)
+
+ 
+ 
+# STEP 3: Access list and dictionary elements
+         # first item in list
+   # get value from dictionary
+
+print("My name is {Dhruvi}")
